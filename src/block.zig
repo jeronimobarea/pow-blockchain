9079c5b7dda0c7ba128bb37fd615bfcf91184ed5
Jeronimo Barea <jeronimobarea@Jeronimos-MacBook-Pro.local>
2023-12-30 14:20:42 +0100

    xd

--- src/block.zig
+++ src/block.zig
@@ -1,25 +1,43 @@
 const std = @import("std");
 
-pub const Blockchain = struct {
-    blocks: []const std.ArrayList(Block),
+const Sha256 = std.crypto.hash.sha2.Sha256;
 
-    pub fn addBlock(self: *Blockchain, block: Block) void {
-        _ = block;
-        _ = self;
-        // Add logic to add a block to the chain
-        // Ensure to validate the block before adding
+const NUM_LEADING_ZEROS_REQUIRED = 4;
+
+pub const BlockchainError = error{
+    InvalidBlock,
+    InvalidProofOfWork,
+};
+
+pub const Blockchain = struct {
+    blocks: std.ArrayList(Block),
+
+    pub fn addBlock(self: *Blockchain, block: Block) !void {
+        if (self.blocks.items.len == 0) {
+            try self.blocks.append(block);
+        } else {
+            try self.validateBlock(block);
+            try self.blocks.append(block);
+        }
+    }
+
+    fn validateBlock(self: *Blockchain, block: Block) !void {
+        const lastBlock = self.blocks.getLast();
+        if (!std.mem.eql(u8, block.previous_hash[0..], lastBlock.hash[0..])) {
+            return BlockchainError.InvalidBlock;
+        }
+
+        if (!isValidHash(block.hash)) {
+            return BlockchainError.InvalidProofOfWork;
+        }
     }
 };
 
 pub const Block = struct {
-    timestamp: u64,
     previous_hash: std.ArrayList(u8),
     hash: std.ArrayList(u8),
     nonce: u64,
-    transactions: std.ArraList(Transaction),
-
-    pub fn genesis() void {}
-    pub fn hash() void {}
+    transactions: std.ArrayList(Transaction),
 };
 
 fn proofOfWork(block: *Block) void {
@@ -30,11 +48,41 @@ fn proofOfWork(block: *Block) void {
 }
 
 fn calculateHash(block: *Block) std.ArrayList(u8) {
-    _ = block;
+    var s = std.ArrayList(u8).init(std.heap.page_allocator);
+    defer s.deinit();
+
+    s.appendSlice(block.previous_hash[0..]);
+    for (block.transactions) |tx| {
+        s.appendSlice(tx.sender[0..]);
+        s.appendSlice(tx.receiver[0..]);
+        s.appendSlice(std.mem.sliceAsBytes(&tx.amount));
+    }
+    s.appendSlice(std.mem.sliceAsBytes(&block.nonce));
+
+    var hash: std.ArraList(u8) = undefined;
+    Sha256.hash(s, hash, null);
+    return hash;
 }
 
-fn isValidHash(hash: std.ArrayList(u8)) bool {
-    _ = hash;
+pub fn isValidHash(hash: std.ArrayList(u8)) bool {
+    var numLeadingZeros: u32 = 0;
+
+    for (hash) |byte| {
+        if (byte == 0) {
+            numLeadingZeros += 8;
+        } else {
+            var mask: u8 = 0x80; // 1000 0000 in binary
+            while (mask != 0 and (byte & mask) == 0) {
+                numLeadingZeros += 1;
+                mask >>= 1;
+            }
+            break;
+        }
+
+        if (numLeadingZeros >= NUM_LEADING_ZEROS_REQUIRED) break;
+    }
+
+    return numLeadingZeros >= NUM_LEADING_ZEROS_REQUIRED;
 }
 
 pub const Transaction = struct {
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,64 @@
 const std = @import("std");
 
-pub fn main() !void {
-    std.debug.print("Hi");
+const block = @import("block.zig");
+
+const Blockchain = block.Blockchain;
+const Block = block.Block;
+const Transaction = block.Transaction;
+
+pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var blockchain = Blockchain{
+        .blocks = std.ArrayList(Block).init(gpa.allocator()),
+    };
+    defer blockchain.blocks.deinit();
+
+    var genesisBlock = Block{
+        .previous_hash = undefined,
+        .transactions = std.ArrayList(Transaction).init(gpa.allocator()),
+        .hash = calculateGenesisHash(),
+        .nonce = 0,
+    };
+    try blockchain.addBlock(genesisBlock);
+
+    printBlockchain(&blockchain);
+}
+
+fn printBlockchain(blockchain: *Blockchain) void {
+    std.debug.print("Blockchain:\n", .{});
+    for (blockchain.blocks.items, 0..) |b, index| {
+        std.debug.print("Block {}: \n", .{index});
+        std.debug.print("    Previous Hash: {}\n", .{b.previous_hash});
+        std.debug.print("    Hash: {}\n", .{b.hash});
+        std.debug.print("    Nonce: {}\n", .{b.nonce});
+    }
+}
+
+pub fn calculateGenesisHash() std.ArrayList(u8) {
+    var s = std.ArrayList(u8).init(std.heap.page_allocator);
+    defer s.deinit();
+
+    // Add some unique data for the genesis block
+    s.appendSlice("Genesis Block".*); // Example data
+    s.appendSlice(u64ToBytes(0)); // Example nonce
+
+    // Simple hash calculation (not cryptographically secure)
+    var hash: std.ArrayList(u8) = undefined;
+    var hashVal: u64 = 0;
+    for (s.items) |b| {
+        // A basic hash function, for example, a rolling hash
+        hashVal = (hashVal * 31) ^ b;
+    }
+
+    // Convert the hash value to a byte array
+    std.mem.writeIntLittle(u64, &hash, hashVal);
+    return hash;
+}
+
+fn u64ToBytes(value: u64) []const u8 {
+    var buffer: [8]u8 = undefined;
+    std.mem.writeIntLittle(u64, &buffer, value);
+    return buffer[0..];
 }
